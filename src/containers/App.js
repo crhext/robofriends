15d815231715f980e76373545cef8c1ad462b86c
crhext <crhext@gmail.com>
2019-10-26 15:51:28 +0100

    adding robofriends project

---
+++ src/containers/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList.js";
+import SearchBox from "../components/Searchbox.js";
+import Scroll from "../components/Scroll.js";
+import "./App.css";
+
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			robots: [],
+			searchfield: ""
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then(response => {
+				return response.json();
+			})
+			.then(users => {
+				this.setState({ robots: users });
+			});
+	}
+
+	onSearchChange = event => {
+		this.setState({ searchfield: event.target.value });
+	};
+
+	render() {
+		const filteredRobots = this.state.robots.filter(robot => {
+			return robot.name
+				.toLowerCase()
+				.includes(this.state.searchfield.toLowerCase());
+		});
+
+		if (this.state.robots.length === 0) {
+			return <h1> Loading </h1>;
+		} else {
+			return (
+				<div className="tc">
+					<h1 className="f1"> RoboFriends </h1>
+					<SearchBox searchChange={this.onSearchChange} />
+					<Scroll id= "scroll">
+						<CardList robots={filteredRobots} />
+					</Scroll>
+				</div>
+			);
+		}
+	}
+}
+
+export default App;
