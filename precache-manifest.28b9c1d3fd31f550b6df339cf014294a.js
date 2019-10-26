6ea13963e7c1bd0c99e5826c7c08b9e1379654cf
crhext <crhext@gmail.com>
2019-10-26 17:42:09 +0100

    Updates

---
+++ precache-manifest.28b9c1d3fd31f550b6df339cf014294a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de5764a3f984413f26d1d50a28013d4",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "60408db5d779033468e9",
+    "url": "/robofriends/static/css/main.4dea9d40.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "60408db5d779033468e9",
+    "url": "/robofriends/static/js/main.45bacbfc.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
