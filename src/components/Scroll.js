3c9deeabea1e28196f40fef03812df13700447a3
crhext <crhext@gmail.com>
2019-10-26 18:54:23 +0100

    updating size of card

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
 	return(
-	<div style = {{overflowY: 'scroll', border: '1px solid black', height: '300px'}}>
+	<div style = {{overflowY: 'scroll', border: '1px solid black', height: '360px'}}>
 		{props.children}
 	</div>
 	)
